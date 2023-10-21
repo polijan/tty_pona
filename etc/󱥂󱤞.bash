c55ebf2a166e487c4b6438b152935e1a7de40a19
polijan <6453311+polijan@users.noreply.github.com>
2023-10-21 14:34:37 +0300

    avoid spawning new processes

--- etc/󱥂󱤞.bash
+++ etc/󱥂󱤞.bash
@@ -8,7 +8,7 @@
 while ! read -r -s -n 1 -t 0.01; do
    kule=$((1 + RANDOM % 255))
    nimi=$((1 + RANDOM % 137))
-   nanpa_nimi=$(printf '%02X' $nimi)
+   printf -v nanpa_nimi '%02X' "$nimi"
    printf '\033[38;5;%dm%b' $kule "\UF19${nanpa_nimi}"
 done
 printf '\033[m\n'
